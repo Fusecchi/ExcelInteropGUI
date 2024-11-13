d193943e9ed5975c8a250e012bbcb0a4370ff9d8
PC <andi.fytra@gmail.com>
2024-11-13 14:37:43 +0900

    Edit and Show the missing value

--- ExcelInteropGUI/Form1.Designer.cs
+++ ExcelInteropGUI/Form1.Designer.cs
@@ -29,74 +29,74 @@
         private void InitializeComponent()
         {
             this.SendButton = new System.Windows.Forms.Button();
-            this.MachineName = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DataSelected = new System.Windows.Forms.Label();
+            this.FileType = new System.Windows.Forms.TextBox();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.EditTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.EditTable)).BeginInit();
             this.SuspendLayout();
             // 
             // SendButton
             // 
             this.SendButton.Location = new System.Drawing.Point(38, 26);
             this.SendButton.Name = "SendButton";
-            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.Size = new System.Drawing.Size(83, 31);
             this.SendButton.TabIndex = 0;
-            this.SendButton.Text = "Send";
+            this.SendButton.Text = "Select File";
             this.SendButton.UseVisualStyleBackColor = true;
             this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
             // 
-            // MachineName
+            // DataSelected
             // 
-            this.MachineName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.MachineName.FormattingEnabled = true;
-            this.MachineName.Location = new System.Drawing.Point(155, 26);
-            this.MachineName.Name = "MachineName";
-            this.MachineName.Size = new System.Drawing.Size(318, 24);
-            this.MachineName.TabIndex = 1;
-            this.MachineName.SelectedIndexChanged += new System.EventHandler(this.MachineName_SelectedIndexChanged);
+            this.DataSelected.AutoSize = true;
+            this.DataSelected.Location = new System.Drawing.Point(35, 95);
+            this.DataSelected.Name = "DataSelected";
+            this.DataSelected.Size = new System.Drawing.Size(93, 16);
+            this.DataSelected.TabIndex = 2;
+            this.DataSelected.Text = "Data Selected";
             // 
-            // label1
+            // FileType
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 92);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.FileType.Enabled = false;
+            this.FileType.Location = new System.Drawing.Point(155, 92);
+            this.FileType.Name = "FileType";
+            this.FileType.ReadOnly = true;
+            this.FileType.Size = new System.Drawing.Size(247, 22);
+            this.FileType.TabIndex = 3;
             // 
-            // textBox1
+            // FileName
             // 
-            this.textBox1.Location = new System.Drawing.Point(155, 92);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(247, 22);
-            this.textBox1.TabIndex = 3;
+            this.FileName.Enabled = false;
+            this.FileName.Location = new System.Drawing.Point(155, 30);
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            this.FileName.Size = new System.Drawing.Size(247, 22);
+            this.FileName.TabIndex = 4;
             // 
-            // dataGridView1
+            // EditTable
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(258, 189);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(215, 169);
-            this.dataGridView1.TabIndex = 4;
+            this.EditTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EditTable.Location = new System.Drawing.Point(155, 155);
+            this.EditTable.Name = "EditTable";
+            this.EditTable.RowHeadersWidth = 51;
+            this.EditTable.RowTemplate.Height = 24;
+            this.EditTable.Size = new System.Drawing.Size(477, 230);
+            this.EditTable.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.MachineName);
+            this.Controls.Add(this.EditTable);
+            this.Controls.Add(this.FileName);
+            this.Controls.Add(this.FileType);
+            this.Controls.Add(this.DataSelected);
             this.Controls.Add(this.SendButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EditTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,10 +105,10 @@
         #endregion
 
         private System.Windows.Forms.Button SendButton;
-        private System.Windows.Forms.ComboBox MachineName;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label DataSelected;
+        private System.Windows.Forms.TextBox FileType;
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.DataGridView EditTable;
     }
 }
 
--- ExcelInteropGUI/Form1.cs
+++ ExcelInteropGUI/Form1.cs
@@ -5,16 +5,21 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Application = Microsoft.Office.Interop.Excel.Application;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Office2016.Drawing.Command;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 namespace ExcelInteropGUI
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog ofd = new OpenFileDialog();
         public Form1()
         {
             InitializeComponent();
@@ -24,13 +29,11 @@ namespace ExcelInteropGUI
         {
             try
             {
-                OpenFileDialog ofd = new OpenFileDialog();
                 ofd.Title = "Select Excel File";
                 ofd.Filter = "Excel Files (*.xls;*.xlsx;*.xlsm;*.csv)|*.xls;*.xlsx;*.xlsm;*.csv";
                 if(ofd.ShowDialog() == DialogResult.OK)
                 {
-                    string fp = ofd.FileName;
-                    MessageBox.Show(fp);
+                    LoadData();
                 }
             }
             catch (Exception ex)
@@ -42,11 +45,67 @@ namespace ExcelInteropGUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MachineName.Items.Add("Komatsu");
-            MachineName.Items.Add("Mitsubishi");
-            MachineName.Items.Add("Astes");
+            LoadData();
         }
 
+        private void LoadData()
+        {
+            string fp = ofd.FileName;
+            string fn = Path.GetFileName(fp);
+            //Debug.WriteLine(fp);
+            if (!string.IsNullOrEmpty(fp))
+            {
+                FileName.Text = fn;
+                using (var workbook = new XLWorkbook(fp))
+                {
+                    var sheet = workbook.Worksheet(1);
+                    var lastRow = sheet.LastRowUsed().RowNumber();
+                    var lastCol = sheet.LastColumnUsed().ColumnNumber();
+                    var DataRange = sheet.Range(2, 1, lastRow, lastCol);
+                    for (int i = 0; i < lastRow-1; i++) {
+                        if (!sheet.Cell(2, 1).Value.Equals(sheet.Cell(2+i,1).Value))
+                        {
+                            MessageBox.Show($"Cell in: {sheet.Cell(2 + i, 1)} has value of: {sheet.Cell(2+i,1).Value}");
+                            fp = null;
+                            FileName.Text = null;
+                            return;
+                        }
+                    }
+                    FileType.Text = sheet.Cell(2,1).Value.ToString();
+                    var CellAddr = new List<int>();
+                    foreach (var cell in DataRange.Cells())
+                    {
+                        if (cell.Value.Equals(0) || cell.IsEmpty())
+                        {
+                            //Debug.WriteLine(cell.Address.RowNumber);
+                            if (!CellAddr.Contains(cell.Address.RowNumber)){
+                                CellAddr.Add(cell.Address.RowNumber);
+                            }
+                        }
+                    }
+                    Debug.WriteLine(CellAddr.Count);
+                    System.Data.DataTable EditData = new System.Data.DataTable();
+                    for(int MakeCol =1; MakeCol<=lastCol;MakeCol++)
+                    {
+                        EditData.Columns.Add(sheet.Cell(1, MakeCol).Value.ToString());
+                    }
+                    foreach (var addr in CellAddr)
+                    {
+                        DataRow row = EditData.NewRow();
+                        for (int i = 0; i < lastCol; i++)
+                        {
+                            row[i] = sheet.Cell(addr, i+1).Value;
+                        }
+                        EditData.Rows.Add(row);
+                    }
+                    EditTable.DataSource = EditData;
+                }
+            }
+        }
+        private void reset()
+        {
+
+        }
         private void MachineName_SelectedIndexChanged(object sender, EventArgs e)
         {
 
