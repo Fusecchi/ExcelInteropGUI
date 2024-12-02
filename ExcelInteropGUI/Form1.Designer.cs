48ebd065e5e0bd1d0cd4c29718c2b9bdfd2cb83b
PC <andi.fytra@gmail.com>
2024-12-02 17:56:54 +0900

    Add Clean adn separate the Json File

--- ExcelInteropGUI/Form1.Designer.cs
+++ ExcelInteropGUI/Form1.Designer.cs
@@ -39,11 +39,12 @@
             this.TargetSheet = new System.Windows.Forms.ComboBox();
             this.TargetName = new System.Windows.Forms.TextBox();
             this.ResetButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.PresetLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.FolderBtn = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // SelectData
@@ -152,14 +153,14 @@
             this.ResetButton.UseVisualStyleBackColor = true;
             this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
-            // label1
+            // PresetLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 156);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "label1";
+            this.PresetLabel.AutoSize = true;
+            this.PresetLabel.Location = new System.Drawing.Point(48, 156);
+            this.PresetLabel.Name = "PresetLabel";
+            this.PresetLabel.Size = new System.Drawing.Size(46, 16);
+            this.PresetLabel.TabIndex = 13;
+            this.PresetLabel.Text = "Preset";
             // 
             // label2
             // 
@@ -201,16 +202,27 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 24);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(786, 400);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.FolderBtn);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PresetLabel);
             this.Controls.Add(this.ResetButton);
             this.Controls.Add(this.TargetName);
             this.Controls.Add(this.TargetSheet);
@@ -244,11 +256,12 @@
         private System.Windows.Forms.ComboBox TargetSheet;
         private System.Windows.Forms.TextBox TargetName;
         private System.Windows.Forms.Button ResetButton;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PresetLabel;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button FolderBtn;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
